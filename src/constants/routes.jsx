af2fb6f2bcfcf51852787d29331418c3f95ca210
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-04-04 01:05:36 +0530

    feat: add apiEndpoints and routes file

---
+++ src/constants/routes.jsx
@@ -0,0 +1,6 @@
+export const HOME_ROUTE = '/';
+export const ALL_RESTURANTS = '/seeOptions';
+export const NOT_FOUND = '*'
+export const BAD_REQUEST_ROUTE = '/bad-request';
+export const SERVER_ERROR_ROUTE = '/server-error';
+export const SOMETHING_WENT_WRONG_ROUTE = '/something-went-wrong';
