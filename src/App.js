ce409dda132cbca62185c74103eb847ec07ac410
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-04-03 22:54:47 +0530

    chore: initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      hello
+    </div>
+  );
+}
+
+export default App;
