6b059fa55b7dc612a163c23682053caacdc7f36d
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-04-05 00:43:38 +0530

    feat: add new endpoints
    fix: routes modified and added

--- src/constants/apiEndPoints.jsx
+++ src/constants/apiEndPoints.jsx
@@ -1,6 +1,16 @@
-export const BACKEND_URL = 'http://localhost:3001';
+export const BACKEND_URL = 'http://localhost:3001/swiggy';
 
 export const getAllRestaurants = {
   method: 'get',
   url: '/seeOptions',
 };
+
+export const getRestaurantMenu = (restId) => ({
+  method: 'get',
+  url: `/seeOptions/${restId}`,
+});
+
+export const getResturantByDish = (dish) =>({
+  method: 'get',
+  url: `/searchOption/${dish}/`,
+})
--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -1,5 +1,6 @@
 export const HOME_ROUTE = '/';
-export const ALL_RESTURANTS = '/seeOptions';
+export const ALL_RESTURANTS = '/swiggy/seeOptions/';
+export const SEE_BY_DISH = '/swiggy/searchOption/';
 export const NOT_FOUND = '*'
 export const BAD_REQUEST_ROUTE = '/bad-request';
 export const SERVER_ERROR_ROUTE = '/server-error';
